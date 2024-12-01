0351fd5e663095552626bbc93a5bde5dbe0f6161
RomanVintoniak <romanvintoniak03@gmail.com>
2024-12-01 20:34:00 +0200

    Add Karma config file

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox'
+        ]
+      }
+    }
+  });
+};
